733cef97b3a3289a79d917b24053eeb2cadbd5bd
François Cami <fcami@redhat.com>
2022-03-09 11:21:38 -0500

    run_robot_test.sh: the artifacts dir should be 777
    Signed-off-by: Erwan Granger <egranger@redhat.com>
    Signed-off-by: François Cami <fcami@redhat.com>

--- run_robot_test.sh
+++ run_robot_test.sh
@@ -139,3 +139,6 @@ case "$(uname -s)" in
 esac
 
 ./venv/bin/robot ${TEST_EXCLUDE_TAG} ${TEST_INCLUDE_TAG} -d ${TEST_ARTIFACT_DIR} -x xunit_test_result.xml -r test_report.html ${TEST_VARIABLES} --variablefile ${TEST_VARIABLES_FILE} --exclude TBC ${EXTRA_ROBOT_ARGS} ${TEST_CASE_FILE}
+
+# make the directory readable post-execution
+chmod -R 777 ${TEST_ARTIFACT_DIR}
